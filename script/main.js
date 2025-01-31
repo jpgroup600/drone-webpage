72ef66d913d76ea74f609b6a7445e3571c077d4f
jpgroup600 <133649099+jpgroup600@users.noreply.github.com>
2025-01-31 15:08:42 +0900

    updated member details popup

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Get all navigation links (excluding the whitepaper link)
     const navLinks = document.querySelectorAll('.navbar_link-el[data-href^="#"]');
-    
+
     const scrollToSection = (sectionId) => {
         const section = document.querySelector(sectionId);
         if (section) {
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', () => {
         link.addEventListener('click', () => {
             const sectionId = link.getAttribute('data-href');
             scrollToSection(sectionId);
-            
+
             // Optional: Update active state
             navLinks.forEach(l => l.classList.remove('active'));
             link.classList.add('active');
@@ -41,7 +41,7 @@ $(document).ready(() => {
         }
     });
 
-    
+
 });
 
 
@@ -72,26 +72,63 @@ const swiper = new Swiper('.swiper2', {
 });
 
 document.addEventListener('DOMContentLoaded', () => {
-    // const teamWrappers = document.querySelectorAll('.team-wrapper');
-    // let isMobile = false;
-    // let styleChange = '-90%'
-
-    // if (window.innerWidth < 768) {
-    //     isMobile = true;
-    //     styleChange = '-85%'
-    // }
+    const teamWrappers = document.querySelectorAll('.team-wrapper');
+    let isMobile = false;
+    let styleChange = '-90%'
 
 
-    // teamWrappers.forEach(wrapper => {
-    //     const labelContainer = wrapper.querySelector('.team-label-container');
-        
-    //         wrapper.addEventListener('mouseenter', () => {
-    //             labelContainer.style.bottom = '0';
-    //         });
+    if (window.innerWidth < 768) {
+        isMobile = true;
+        styleChange = '-85%'
+    }
 
-    //         wrapper.addEventListener('mouseleave', () => {
-    //             labelContainer.style.bottom = styleChange;
-    //         });
-    // });
+
+    teamWrappers.forEach((wrapper, index) => {
+    
+    const content = [
+        `Air Force Academy graduate <br>
+F-15K IWSO, Fighter Weapons Instructor Course <br>
+Commercial Pilot, CE-525 Type Rating <br>
+Curator, National Aviation Museum`,
+
+"",
+
+`ESG Consultant <br>
+Drone Safety Manager <br>
+AIRON CMO <br>
+HEARIM CEO <br>
+AIRLABS CSO`,
+
+"",
+
+
+    ]
+    const labelContainer = wrapper.querySelector('.team-label-container');
+    const popup = document.querySelector('.team-cont-popup');
+    const popupClose = document.querySelector('.team-cont-popup-close');
+
+    popup.style.opacity = '0';
+    popup.style.scale = '0';
+
+    wrapper.addEventListener('mouseenter', () => {
+        labelContainer.style.bottom = '0';
+    });
+
+    wrapper.addEventListener('mouseleave', () => {
+        labelContainer.style.bottom = styleChange;
+    });
+
+    wrapper.addEventListener('click', () => {
+        popup.style.opacity = '1';
+        popup.style.scale = '1';
+
+        popup.querySelector('.team-cont-popup-text').innerHTML = content[index];
+    });
+
+    popupClose.addEventListener('click', () => {
+        popup.style.opacity = '0';
+        popup.style.scale = '0';
+    });
+});
 
 });
